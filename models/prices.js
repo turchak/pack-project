44bb79d7ec144a30617a514b86b4fe34d27218db
turchak <turchaksv@gmail.com>
2017-10-17 23:46:07 +0300

    deleted xoauth2

--- models/prices.js
+++ models/prices.js
@@ -1,7 +1,7 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+let Schema = mongoose.Schema;
 
-var pricesSchema = new Schema({
+let pricesSchema = new Schema({
   name: String,
   price: Number
 });
